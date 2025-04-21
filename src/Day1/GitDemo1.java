3239a8536e9c2f3d37088dc788608e9227bd7a94
Prakhar Jain <prakhar.jnt@gmail.com>
2025-04-21 10:45:14 +0530

    my first commit

---
+++ src/Day1/GitDemo1.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class GitDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	This is first sample class file for GIT learnings
+	}
+
+}
